d40fb7077c827eb8cd2afd80eb4b2c5f5ae1ca1b
Elizabeth Young <e.young1980@gmail.com>
2016-04-26 13:46:12 +0100

    Implemented MoveRule

---
+++ IceWarpLib/IceWarpLib.Rpc/Requests/Rule/MoveRule.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml;
+using IceWarpLib.Objects.Helpers;
+using IceWarpLib.Objects.Rpc.Enums;
+using IceWarpLib.Rpc.Exceptions;
+using IceWarpLib.Rpc.Responses;
+using IceWarpLib.Rpc.Utilities;
+
+namespace IceWarpLib.Rpc.Requests.Rule
+{
+    /// <summary>
+    /// Move specified Rule up or down. See <see cref="IceWarpCommand{SuccessResponse}"/> for return type.
+    /// </summary>
+    public class MoveRule : IceWarpCommand<SuccessResponse>
+    {
+        /// <summary>
+        /// The value can be email address(account rules), domain name(domain rules) or empty string (server rules)
+        /// </summary>
+        public string Who { get; set; }
+        /// <summary>
+        /// Id of the rule
+        /// </summary>
+        public int RuleID { get; set; }
+        /// <summary>
+        /// Specifies whether to move rule up or down. <see cref="TRuleMoveType"/>
+        /// </summary>
+        public TRuleMoveType MoveType { get; set; }
+
+        protected override void BuildCommandParams(XmlDocument doc, XmlElement command)
+        {
+            var commandParams = XmlHelper.CreateElement(doc, "commandparams");
+
+            XmlHelper.AppendTextElement(commandParams, "Who", Who);
+            XmlHelper.AppendTextElement(commandParams, "RuleID", RuleID);
+            XmlHelper.AppendTextElement(commandParams, "MoveType", MoveType);
+
+            command.AppendChild(commandParams);
+        }
+
+        /// <summary>
+        /// Generates the response from the HTTP request result.
+        /// </summary>
+        /// <param name="httpRequestResult">The HTTP request result.</param>
+        /// <returns>The response from IceWarp. See <see cref="SuccessResponse"/> for more information.</returns>
+        /// <exception cref="ProcessResponseException"> Thrown if HttpRequestResult is null, if HttpRequestResult.Response is null or empty or an exception occurs when loading the XML.</exception>
+        /// <exception cref="IceWarpErrorException">Thrown if IceWarp returned and error.</exception>
+        public override SuccessResponse FromHttpRequestResult(HttpRequestResult httpRequestResult)
+        {
+            return new SuccessResponse(httpRequestResult);
+        }
+    }
+}
--- IceWarpLib/IceWarpLib.UnitTests/IceWarpRpc/Requests/Rules/RulesRequestTests.cs
+++ IceWarpLib/IceWarpLib.UnitTests/IceWarpRpc/Requests/Rules/RulesRequestTests.cs
@@ -226,5 +226,28 @@ namespace IceWarpLib.UnitTests.IceWarpRpc.Requests.Rules
             Assert.AreEqual(typeof(TRuleTrustedSessionCondition), response.Items.First().Condition.GetType());
             Assert.AreEqual(typeof(TRuleHasAttachmentCondition), response.Items.Last().Condition.GetType());
         }
+
+        [Test]
+        public void MoveRule()
+        {
+            string expected = File.ReadAllText(Path.Combine(_requestsTestDataPath, "MoveRule.xml"));
+            var request = new MoveRule
+            {
+                SessionId = "sid",
+                Who = "test@testing.com",
+                RuleID = 1,
+                MoveType = TRuleMoveType.Down
+            };
+            var xml = request.ToXml().InnerXmlFormatted();
+            Assert.AreEqual(expected, xml);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(File.ReadAllText(Path.Combine(_responsesTestDataPath, "MoveRule.xml")));
+            var response = request.FromHttpRequestResult(new HttpRequestResult { Response = doc.InnerXml });
+
+            Assert.AreEqual("result", response.Type);
+            Assert.True(response.Success);
+        }
+
     }
 }
